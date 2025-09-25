9e39cd4145398d3a82a7b9e04d903869c6739d50
RajatSaini04 <rajatsaini0411@gmail.com>
2025-09-26 01:05:47 +0530

    Added Few More Patterns

--- JS-Patterns-Print/StarPattern.js
+++ JS-Patterns-Print/StarPattern.js
@@ -18,7 +18,7 @@
 
 
 // // star 2 
-// // * * * * *
+// // 
 // // * * * *
 // // * * *
 // // * *
@@ -35,6 +35,7 @@
 // console.log(row2);
 
 
+let n = 5
 
 // STAR 1
 // ****
@@ -80,8 +81,9 @@ function star3() {
     for (let i = 0; i < 4; i++) {
         let row = ""; // build one row
         for (let j = 0; j <= i; j++) {
-            row += j + " ";  //cols print ho rhe ha j se
-        }
+            row += j + " ";  //cols print ho rhe ha j se jo ke
+        }                    // 1 se i ki value tk chal rhe ha 
+        //that prints counting like 1234 etc...
         console.log(row); // print full row
     }
 }
@@ -98,7 +100,7 @@ function star4() {
         let row = ""; // build one row
         for (let j = 0; j <= i; j++) {
             row += i + " ";        //Row print ho rhi ha i se
-        }
+        }   // i ki val static ha no of time j run ho rha ha, jabhi ye 1 ,22 , 333 ho rhe ha print
         console.log(row); // print full row
     }
 }
@@ -110,23 +112,153 @@ star4()
 // **
 // *
 console.log('Star 5');
+// logic made by me
+// function star5() {
+//     for (let i = 0; i < 4; i++) {
+//         let row = ""; // build one row
+//         for (let j = 4; j > i; j--) {
+//             row += "*";        //Row print ho rhi ha i se
+//         }
+//         console.log(row); // print full row
+//     }
+// }
+// star5()
+
+// start5 by striver -- 
 function star5() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 1; i <= 4; i++) {
         let row = ""; // build one row
-        for (let j = 4; j > i; j--) {
-            row += "*";        //Row print ho rhi ha i se
+        for (let j = 0; j < 4 - i + 1; j++) {
+            row += "*"; // har i pr n+1 me - i , run hoga
         }
         console.log(row); // print full row
     }
 }
 star5()
+
+
+// Star 6
+// 12345
+// 1234
+// 123
+// 12
+// 1
+console.log('star 6');
 function star6() {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= n; i++) {
         let row = ""; // build one row
-        for (let j = 0; j<4-i+1; j++) {
-            row += "*";        //Row print ho rhi ha i se
+        for (let j = 1; j <= n + 1 - i; j++) {
+            row += j + " ";        //Row print ho rhi ha i se
         }
         console.log(row); // print full row
     }
 }
 star6()
+
+// Star 7
+
+//      *     
+//     ***    
+//    *****   
+//   *******  
+//  ********* 
+// ***********
+console.log('star 7');
+function star7() {
+    for (let i = 1; i <= n; i++) {
+        let row = ""; // build one row
+        // left spaces
+        for (let j = 1; j < n + 1 - i; j++) {
+            row += j;        //spaces print
+        } // same for right spaces but not needed
+
+        for (let j = 1; j <= 2 * i - 1; j++) {
+            row += "*";        //Star print
+        }
+        console.log(row); // print full row
+    }
+}
+star7()
+
+// star 8
+//  *********
+//   *******
+//    ***** 
+//     ***    
+//      *
+console.log('');
+console.log("star 8");
+function star8() {
+    for (let i = 1; i <= n; i++) {
+
+        let row = ""; // build one row
+        // left spaces
+        for (let j = 1; j < i; j++) {
+            row += j;        //spaces print
+        } // same for right spaces but not needed
+
+        for (let j = 1; j <= 2 * (n - i) + 1; j++) {
+            row += "*";        //Star print
+        }
+        console.log(row); // print full row
+    }
+}
+star8();
+
+// console.log('');
+// console.log('star ');
+function star9() { }
+star9();
+// console.log('');
+// console.log('star ');
+function star10() { }
+star10();
+// console.log('');
+// console.log('star ');
+function star11() { }
+star11();
+// console.log('');
+// console.log('star ');
+function star12() { }
+star12();
+// console.log('');
+// console.log('star ');
+function star13() { }
+star13();
+// console.log('');
+// console.log('star ');
+function star14() { }
+star14();
+// console.log('');
+// console.log('star ');
+function star15() { }
+star15();
+// console.log('');
+// console.log('star ');
+function star16() { }
+star16();
+// console.log('');
+// console.log('star ');
+function star17() { }
+star17();
+// console.log('');
+// console.log('star ');
+function star18() { }
+star18();
+// console.log('');
+// console.log('star ');
+function star19() { }
+star19();
+// console.log('');
+// console.log('star ');
+function star20() { }
+star20();
+// console.log('');
+// console.log('star ');
+function star21() { }
+star21();
+// console.log('');
+// console.log('star ');
+function star22() { }
+star22();
+
